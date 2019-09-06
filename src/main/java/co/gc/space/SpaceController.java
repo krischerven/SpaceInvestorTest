9014286c5e021fa931e1073632d3bef7ff195b9e
Kris Cherven <krischerven@gmail.com>
2019-09-06 15:37:03 -0400

    Update SpaceController.java

--- src/main/java/co/gc/space/SpaceController.java
+++ src/main/java/co/gc/space/SpaceController.java
@@ -1,12 +1,16 @@
 package co.gc.space;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import co.gc.space.entity.planet.Planet;
 
 @Controller
 public class SpaceController {
 	@RequestMapping("/")
 	public ModelAndView home() {
-		return new ModelAndView("index", "response", null);
+		List<Planet> planets = new ArrayList<>();
+		return new ModelAndView("index", "response", planets);
 	}
 }
