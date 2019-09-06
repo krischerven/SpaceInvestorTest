13b57b11f8a1a658c878f98fbab7869c72e74167
sean <seanhcyang10@gmail.com>
2019-09-06 14:56:52 -0400

    changed thing in the planet pojo

--- src/main/java/co/gc/space/entity/planet/Planet.java
+++ src/main/java/co/gc/space/entity/planet/Planet.java
@@ -1,104 +1,207 @@
 package co.gc.space.entity.planet;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Planet {
 
-	private String name;
-	private Boolean status;
-	private Short discovered;
-	private Long mass;
-	private Float orbital;
-	private Float radius;
-	private Float temperature;
-	private Long distance;
-	private List<String> names;
+	private String planetName;
+	private String starName;
+	private String publicationStatus;
+	private String yearDiscovered;
+	private String mass;
+	private String massErrorMin;
+	private String massErrorMax;
+	private String radius;
+	private String radiusErrorMin;
+	private String radiusErrorMax;
+	private String period;
+	private String periodErrorMin;
+	private String periodErrorMax;
+	private String tempCalculated;
+	private String tempMeasured;
+	private String starDistance;
+	private String starDistanceErrorMin;
+	private String starDistanceErrorMax;
+	private String inclination;
+	private String inclinationErrorMin;
 
 	public Planet() {
 		super();
 		// TODO Auto-generated constructor stub
+	
+}
+
+	public String getPlanetName() {
+		return planetName;
 	}
 
-	public String getName() {
-		return name;
+	public void setPlanetName(String planetName) {
+		this.planetName = planetName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getStarName() {
+		return starName;
 	}
 
-	public Boolean getStatus() {
-		return status;
+	public void setStarName(String starName) {
+		this.starName = starName;
 	}
 
-	public void setStatus(Boolean status) {
-		this.status = status;
+	public String getPublicationStatus() {
+		return publicationStatus;
 	}
 
-	public Short getDiscovered() {
-		return discovered;
+	public void setPublicationStatus(String publicationStatus) {
+		this.publicationStatus = publicationStatus;
 	}
 
-	public void setDiscovered(Short discovered) {
-		this.discovered = discovered;
+	public String getYearDiscovered() {
+		return yearDiscovered;
 	}
 
-	public Long getMass() {
+	public void setYearDiscovered(String yearDiscovered) {
+		this.yearDiscovered = yearDiscovered;
+	}
+
+	public String getMass() {
 		return mass;
 	}
 
-	public void setMass(Long mass) {
+	public void setMass(String mass) {
 		this.mass = mass;
 	}
 
-	public Float getOrbital() {
-		return orbital;
+	public String getMassErrorMin() {
+		return massErrorMin;
 	}
 
-	public void setOrbital(Float orbital) {
-		this.orbital = orbital;
+	public void setMassErrorMin(String massErrorMin) {
+		this.massErrorMin = massErrorMin;
 	}
 
-	public Float getRadius() {
+	public String getMassErrorMax() {
+		return massErrorMax;
+	}
+
+	public void setMassErrorMax(String massErrorMax) {
+		this.massErrorMax = massErrorMax;
+	}
+
+	public String getRadius() {
 		return radius;
 	}
 
-	public void setRadius(Float radius) {
+	public void setRadius(String radius) {
 		this.radius = radius;
 	}
 
-	public Float getTemperature() {
-		return temperature;
+	public String getRadiusErrorMin() {
+		return radiusErrorMin;
 	}
 
-	public void setTemperature(Float temperature) {
-		this.temperature = temperature;
+	public void setRadiusErrorMin(String radiusErrorMin) {
+		this.radiusErrorMin = radiusErrorMin;
 	}
 
-	public Long getDistance() {
-		return distance;
+	public String getRadiusErrorMax() {
+		return radiusErrorMax;
 	}
 
-	public void setDistance(Long distance) {
-		this.distance = distance;
+	public void setRadiusErrorMax(String radiusErrorMax) {
+		this.radiusErrorMax = radiusErrorMax;
 	}
 
-	public List<String> getNames() {
-		return names;
+	public String getPeriod() {
+		return period;
 	}
 
-	public void setNames(List<String> names) {
-		this.names = names;
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getPeriodErrorMin() {
+		return periodErrorMin;
+	}
+
+	public void setPeriodErrorMin(String periodErrorMin) {
+		this.periodErrorMin = periodErrorMin;
+	}
+
+	public String getPeriodErrorMax() {
+		return periodErrorMax;
+	}
+
+	public void setPeriodErrorMax(String periodErrorMax) {
+		this.periodErrorMax = periodErrorMax;
+	}
+
+	public String getTempCalculated() {
+		return tempCalculated;
+	}
+
+	public void setTempCalculated(String tempCalculated) {
+		this.tempCalculated = tempCalculated;
+	}
+
+	public String getTempMeasured() {
+		return tempMeasured;
+	}
+
+	public void setTempMeasured(String tempMeasured) {
+		this.tempMeasured = tempMeasured;
+	}
+
+	public String getStarDistance() {
+		return starDistance;
+	}
+
+	public void setStarDistance(String starDistance) {
+		this.starDistance = starDistance;
+	}
+
+	public String getStarDistanceErrorMin() {
+		return starDistanceErrorMin;
+	}
+
+	public void setStarDistanceErrorMin(String starDistanceErrorMin) {
+		this.starDistanceErrorMin = starDistanceErrorMin;
+	}
+
+	public String getStarDistanceErrorMax() {
+		return starDistanceErrorMax;
+	}
+
+	public void setStarDistanceErrorMax(String starDistanceErrorMax) {
+		this.starDistanceErrorMax = starDistanceErrorMax;
+	}
+
+	public String getInclination() {
+		return inclination;
+	}
+
+	public void setInclination(String inclination) {
+		this.inclination = inclination;
+	}
+
+	public String getInclinationErrorMin() {
+		return inclinationErrorMin;
+	}
+
+	public void setInclinationErrorMin(String inclinationErrorMin) {
+		this.inclinationErrorMin = inclinationErrorMin;
 	}
 
 	@Override
 	public String toString() {
-		return "Planet [name=" + name + ", status=" + status + ", discovered=" + discovered + ", mass=" + mass
-				+ ", orbital=" + orbital + ", radius=" + radius + ", temperature=" + temperature + ", distance="
-				+ distance + ", names=" + names + "]";
+		return "Planet [planetName=" + planetName + ", starName=" + starName + ", publicationStatus="
+				+ publicationStatus + ", yearDiscovered=" + yearDiscovered + ", mass=" + mass + ", massErrorMin="
+				+ massErrorMin + ", massErrorMax=" + massErrorMax + ", radius=" + radius + ", radiusErrorMin="
+				+ radiusErrorMin + ", radiusErrorMax=" + radiusErrorMax + ", period=" + period + ", periodErrorMin="
+				+ periodErrorMin + ", periodErrorMax=" + periodErrorMax + ", tempCalculated=" + tempCalculated
+				+ ", tempMeasured=" + tempMeasured + ", starDistance=" + starDistance + ", starDistanceErrorMin="
+				+ starDistanceErrorMin + ", starDistanceErrorMax=" + starDistanceErrorMax + ", inclination="
+				+ inclination + ", inclinationErrorMin=" + inclinationErrorMin + "]";
 	}
 
 }
