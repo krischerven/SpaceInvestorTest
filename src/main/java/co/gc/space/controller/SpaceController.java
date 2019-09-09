a11ca03ba2a748774fdd8c6b64dff55a015fcbc7
Kris Cherven <krischerven@gmail.com>
2019-09-09 11:59:28 -0400

    displays the planets

--- src/main/java/co/gc/space/controller/SpaceController.java
+++ src/main/java/co/gc/space/controller/SpaceController.java
@@ -1,16 +1,27 @@
 package co.gc.space.controller;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import co.gc.space.entity.planet.Planet;
+import co.gc.space.entity.planet.*;
+import co.gc.space.repo.PlanetRepo;
 
 @Controller
 public class SpaceController {
+	@Autowired
+	PlanetRepo repo;
 	@RequestMapping("/")
 	public ModelAndView home() {
 		List<Planet> planets = new ArrayList<>();
+		planets.add(new Jupiter());
+		planets.add(new Mars());
+		planets.add(new Mercury());
+		planets.add(new Neptune());
+		planets.add(new Saturn());
+		planets.add(new Uranus());
+		planets.add(new Venus());
 		return new ModelAndView("index", "planetList", planets);
 	}
 }
