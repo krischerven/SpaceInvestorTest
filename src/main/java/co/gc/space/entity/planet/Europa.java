6b594585b6f1aa9b08bed73f26e7d179fea00c38
Kris Cherven <krischerven@gmail.com>
2019-09-11 16:32:56 -0400

    Adds Europa (needs unique description)

---
+++ src/main/java/co/gc/space/entity/planet/Europa.java
@@ -0,0 +1,25 @@
+package co.gc.space.entity.planet;
+
+// not a planet or a solar system planet, but we know more about Europa than we know about exoplanets
+public class Europa extends SolarSystemPlanet {
+
+	public Europa() {
+		super();
+		setPlanetName("Europa");
+		setStarName("Sun");
+		setYearDiscovered("1610");
+		setMass("4.7998 ×10^22 kg");
+		setRadius("969.84 mi");
+		setPeriod("3 1/2 days");
+		setTempCalculated("111 K");
+		setMinTemp("50 K");
+		setMaxTemp("111 K");
+		setStarDistance("484 Million Miles");
+
+		setImageUrl("https://i.dlpng.com/static/png/1131091_preview_preview.png");
+		setJspTag("planets/europa");
+		setGifImage("../images/europaspin.gif");
+
+	}
+
+}
