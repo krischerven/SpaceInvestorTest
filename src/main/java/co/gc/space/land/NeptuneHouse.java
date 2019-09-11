fbbabc5ba988d53ffe829f52bd4d1ad1240f3aee
sean <seanhcyang10@gmail.com>
2019-09-11 16:03:41 -0400

    removed () from the house pojos

--- src/main/java/co/gc/space/land/NeptuneHouse.java
+++ src/main/java/co/gc/space/land/NeptuneHouse.java
@@ -3,12 +3,12 @@ package co.gc.space.land;
 public class NeptuneHouse extends Land {
 
 	public NeptuneHouse() {
-		price = ("20_000_000_000_000l");
-		arces = ("50");
-		propertyDetails = ("High winds blowing, gases around.");
-		houseFeatures = ("4 bedrooms, 3 bathrooms, 1500sqft(inside a dome that's inforced)");
-		architecturalStyle = ("Greek Revival");
-		condition = ("New");
-		yearBuilt = ("2040");
+		price = "20_000_000_000_000l";
+		arces = "50";
+		propertyDetails = "High winds blowing, gases around.";
+		houseFeatures = "4 bedrooms, 3 bathrooms, 1500sqft(inside a dome that's inforced)";
+		architecturalStyle = "Greek Revival";
+		condition = "New";
+		yearBuilt = "2040";
 	}
 }
--- src/main/java/co/gc/space/land/SaturnHouse.java
+++ src/main/java/co/gc/space/land/SaturnHouse.java
@@ -3,12 +3,12 @@ package co.gc.space.land;
 public class SaturnHouse extends Land {
 
 	public SaturnHouse() {
-		price = ("7_000_000_000_000l");
-		arces = ("45");
-		propertyDetails = ("No solid surface.");
-		houseFeatures = ("4 bedrooms, 3 bathrooms, 2400sqft(inside a dome off the ground)");
-		architecturalStyle = ("Tudor");
-		condition = ("New");
-		yearBuilt = ("2040");
+		price = "7_000_000_000_000l";
+		arces = "45";
+		propertyDetails = "No solid surface.";
+		houseFeatures = "4 bedrooms, 3 bathrooms, 2400sqft(inside a dome off the ground)";
+		architecturalStyle = "Tudor";
+		condition = "New";
+		yearBuilt = "2040";
 	}
 }
