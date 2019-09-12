0bbef890885a841db915f196e544dffa29a6ea16
Kris Cherven <krischerven@gmail.com>
2019-09-12 09:45:07 -0400

    Update Planet.java

--- src/main/java/co/gc/space/entity/planet/Planet.java
+++ src/main/java/co/gc/space/entity/planet/Planet.java
@@ -267,7 +267,7 @@ public class Planet {
 	@Override
 	public String toString() {
 		return "<h2>" + planetName + "</h2><a href=" + getJspTag() + "><img src=" + getImageUrl()
-				+ " + alt=ERROR width=600px height=600px></a>";
+				+ " + alt=ERROR width=500px height=500px></a>";
 	}
 
 }
