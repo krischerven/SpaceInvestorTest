9fe8df3648cfbe5df95c5baf09c836146c02824a
Kris Cherven <krischerven@gmail.com>
2019-09-12 10:22:21 -0400

    Update mars_graph1.js

--- src/main/resources/static/scripts/mars_graph1.js
+++ src/main/resources/static/scripts/mars_graph1.js
@@ -7,14 +7,14 @@ graph.x = document.getElementById("xPoints_1").innerText.split(",");
 graph.y = document.getElementById("yPoints_1").innerText.split(",");
 
 let layout = {
-		title: 'Price Projections of Sending A Rocket To Mars',
+		title: 'ERROR',
 		xaxis: {
-			title: 'Year',
+			title: 'ERROR',
 			showgrid: false,
 			zeroline: false
 		},
 		yaxis: {
-			title: 'Price (Billion $)',
+			title: 'ERROR',
 			showline: false
 		},
 };
