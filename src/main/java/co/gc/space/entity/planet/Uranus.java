93858857b1800f5970c171849551cdf7ea3a243f
sean <seanhcyang10@gmail.com>
2019-09-13 15:52:45 -0400

    fixed venus house picture

--- src/main/java/co/gc/space/entity/planet/Uranus.java
+++ src/main/java/co/gc/space/entity/planet/Uranus.java
@@ -18,7 +18,7 @@ public class Uranus extends SolarSystemPlanet {
 		setImageUrl("https://space-facts.com/wp-content/uploads/uranus-transparent.png");
 		setJspTag("planets/uranus");
 		setGifImage("../images/uranusspin.gif");
-		setHouseImage("../images/craftsmanhouse");
+		setHouseImage("../images/craftsmanhouse.jpg");
 	}
 
 }
