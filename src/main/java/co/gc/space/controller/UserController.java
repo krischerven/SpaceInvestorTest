7770ccafaa6e26f528fe40a5c94758585fd693aa
Kris Cherven <krischerven@gmail.com>
2019-09-13 15:38:01 -0400

    UserController.java refactors

--- src/main/java/co/gc/space/controller/UserController.java
+++ src/main/java/co/gc/space/controller/UserController.java
@@ -1,20 +1,20 @@
-package co.gc.space.user;
-
+package co.gc.space.controller;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import co.gc.space.user.CreditCard;
+import co.gc.space.user.User;
+import co.gc.space.user.UserRepo;
 
 @Controller
 public class UserController {
 
 	@Autowired
-	UserRepo a;
+	UserRepo repo;
 
 	@RequestMapping("create-user")
 	public ModelAndView addUser() {
@@ -29,7 +29,7 @@ public class UserController {
 	@RequestMapping("logged-in") 
 	public ModelAndView loggedIn(@RequestParam("email") String email, @RequestParam("password") String password) {
 		ModelAndView mv = new ModelAndView("logged-in");
-		Optional<User> user = a.findByEmail(email);
+		Optional<User> user = repo.findByEmail(email);
 		try {
 			mv.addObject("account", user.get());
 		} catch (NoSuchElementException e) {
@@ -43,13 +43,14 @@ public class UserController {
 		if (user.getCreditcard() != null && user.getCvv() != null) {
 			String test = CreditCard.getMatchingCreditCard(user.getCreditcard(), user.getCvv());
 			System.out.println(test);
-			if (test != null)
-				a.save(user);
-			if (test == null)
+			if (test == null) {
 				return new ModelAndView("error-message");
-
+			} else {
+				repo.save(user);
+				return new ModelAndView("user-created");
+			}
+		} else {
+			return new ModelAndView("user-created");
 		}
-	
-		return new ModelAndView("user-created");
 	}
 }
